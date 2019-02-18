48a4b746bb9c2d432568b7ad51ff67ae133692e2
hank <hank.huiwang@gmail.com>
2019-02-18 22:31:30 +0800

    个人信息接口返回行程信息

--- app/Http/Controllers/Api/Account/ProfileController.php
+++ app/Http/Controllers/Api/Account/ProfileController.php
@@ -1,5 +1,6 @@
 <?php namespace App\Http\Controllers\Api\Account;
 use App\Http\Controllers\Api\Controller;
+use App\Models\Transport\TransportLbs;
 use App\Models\Transport\TransportSub;
 use Illuminate\Http\Request;
 /**
@@ -15,7 +16,14 @@ class ProfileController extends Controller {
         $sub = TransportSub::where('api_user_id', $user->id)->whereIn('transport_status',[TransportSub::TRANSPORT_STATUS_PENDING,TransportSub::TRANSPORT_STATUS_PROCESSING])->first();
         $data = $user->toArray();
         $data['transport_sub_id'] = '';
+        $data['need_upload_positions'] = false;
         if ($sub) {
+            if ($sub->transport_status == TransportSub::TRANSPORT_STATUS_PROCESSING) {
+                $lastLbs = TransportLbs::where('transport_sub_id',$sub->id)->orderBy('id','desc')->first();
+                if (!$lastLbs || $lastLbs->created_at <= date('Y-m-d H:i:s',strtotime('-70 seconds'))) {
+                    $data['need_upload_positions'] = true;
+                }
+            }
             $data['transport_sub_id'] = $sub->id;
         }
         return self::createJsonData(true,$data);
