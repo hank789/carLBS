a45abe551d05f2ae11294a0f9387a937a92467e3
hank <hank.huiwang@gmail.com>
2019-03-15 18:06:54 +0800

    名字修改

--- map/script/modules/trackcontrol/stores/trackStore.js
+++ map/script/modules/trackcontrol/stores/trackStore.js
@@ -137,7 +137,7 @@ var TrackStore = Reflux.createStore({
         this.trigger('switchmanagetab', index);
     },
     onGetservicename: function() {
-        this.trigger('servicename', '示例DEMO');
+        this.trigger('servicename', '车辆位置');
         // 车辆行业0，
         this.trigger('servicetype', 0);
     },
