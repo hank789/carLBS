b1c9c856f0ec9865d58985d5297f53fe85fa7068
hank <hank.huiwang@gmail.com>
2019-03-19 14:48:58 +0800

    备份

--- config/backup.php
+++ config/backup.php
@@ -18,7 +18,7 @@ return [
                  * The list of directories and files that will be included in the backup.
                  */
                 'include' => [
-                    base_path(),
+                    
                 ],
 
                 /*
@@ -29,6 +29,7 @@ return [
                 'exclude' => [
                     base_path('vendor'),
                     base_path('node_modules'),
+                    base_path('public'),
                 ],
 
                 /*
