1590f608fa45a5df38e6550b2da98785b84d8665
hank <hank.huiwang@gmail.com>
2019-05-09 19:58:25 +0800

    基本报表

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transport\TransportEntity;
+use App\Models\Transport\TransportMain;
+use Carbon\Carbon;
 
 /**
  * Class DashboardController.
@@ -14,6 +17,56 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('backend.dashboard');
+        //车辆数
+        $carsCount = TransportEntity::count();
+        //行程总数
+        $mainCount = TransportMain::count();
+        //行程已完成数
+        $mainFinishedCount = TransportMain::where('transport_status',TransportMain::TRANSPORT_STATUS_FINISH)->count();
+        //行程未完成数
+        $mainUnFinishedCount = $mainCount - $mainFinishedCount;
+
+        $transportChart = $this->drawTransportChart();
+
+        return view('backend.dashboard')->with(compact('carsCount','mainCount','mainFinishedCount','mainUnFinishedCount','transportChart'));
+    }
+
+    private function drawTransportChart()
+    {
+
+        /*生成Labels*/
+        $labelTimes = $chartLabels = [];
+
+        for( $i=0 ; $i < 30 ; $i++ ){
+            $labelTimes[$i] = Carbon::createFromTimestamp( Carbon::today()->timestamp - (29-$i) * 24 * 3600 );
+            $chartLabels[$i] = '"'.$labelTimes[$i]->month.'月-'.$labelTimes[$i]->day.'日'.'"';
+        }
+
+        $nowTime = Carbon::now();
+
+        $mainList = TransportMain::where('transport_start_time','>',$labelTimes[0])->where('transport_start_time','<',$nowTime)->get();
+
+        $totalRange = $unFinishedRange = $finishedRange = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+        for( $i=0 ; $i < 30 ; $i++ ){
+            $startTime = $labelTimes[$i];
+            $endTime = $nowTime;
+            if(isset($labelTimes[$i+1])){
+                $endTime = $labelTimes[$i+1];
+            }
+
+            foreach($mainList as $main){
+                if( $main->transport_start_time >= $startTime && $main->transport_start_time < $endTime ){
+                    $totalRange[$i]++;
+                    if($main->transport_status == TransportMain::TRANSPORT_STATUS_FINISH ){
+                        $finishedRange[$i]++;
+                    } else {
+                        $unFinishedRange[$i]++;
+                    }
+                }
+            }
+
+        }
+        return ['labels'=>$chartLabels,'totalRange'=>$totalRange,'unFinishedRange'=>$unFinishedRange, 'finishedRange'=>$finishedRange];
     }
 }
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -3,16 +3,129 @@
 @section('title', app_display_name() . ' | ' . __('strings.backend.dashboard.title'))
 
 @section('content')
-    <div class="row">
-        <div class="col">
+
             <div class="card">
-                <div class="card-header">
-                    <strong>@lang('strings.backend.dashboard.welcome') {{ $logged_in_user->name }}!</strong>
-                </div><!--card-header-->
                 <div class="card-body">
-                    欢迎来到长江智链管理后台！
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="callout callout-success">
+                                        <small class="text-muted">车辆数</small>
+                                        <br>
+                                        <strong class="h4">{{ $carsCount }}</strong>
+                                        <div class="chart-wrapper">
+                                            <canvas id="sparkline-chart-1" width="100" height="30"></canvas>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="callout callout-info">
+                                        <small class="text-muted">行程总数</small>
+                                        <br>
+                                        <strong class="h4">{{ $mainCount }}</strong>
+                                        <div class="chart-wrapper">
+                                            <canvas id="sparkline-chart-1" width="100" height="30"></canvas>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="callout callout-success">
+                                        <small class="text-muted">已完成行程数</small>
+                                        <br>
+                                        <strong class="h4">{{ $mainFinishedCount }}</strong>
+                                        <div class="chart-wrapper">
+                                            <canvas id="sparkline-chart-1" width="100" height="30"></canvas>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="callout callout-danger">
+                                        <small class="text-muted">未完成行程数</small>
+                                        <br>
+                                        <strong class="h4">{{ $mainUnFinishedCount }}</strong>
+                                        <div class="chart-wrapper">
+                                            <canvas id="sparkline-chart-1" width="100" height="30"></canvas>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="mt-0">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="box box-default">
+                                <div class="box-header with-border">
+                                    <h6 class="box-title">近30天数据报告</h6>
+                                </div>
+                                <div class="box-body">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="chart-wrapper">
+                                                <canvas id="transport_chart" height="400"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div><!--card-body-->
             </div><!--card-->
-        </div><!--col-->
-    </div><!--row-->
+
+@endsection
+@section('script')
+    {!! script(('js/plugins/chartjs/Chart.min.js'),[],config('app.use_ssl')) !!}
+    <script type="text/javascript">
+        $(document).ready(function() {
+            var transportChart = new Chart($("#transport_chart"), {
+                type: 'line',
+                data: {
+                    labels: [{!! implode(",",$transportChart['labels']) !!}],
+                    datasets: [
+                        {
+                            label: '行程总数',
+                            backgroundColor: "#17a2b8",
+                            borderColor: "#17a2b8",
+                            fill: false,
+                            data: [{{ implode(",",$transportChart['totalRange']) }}]
+                        },
+                        {
+                            fill: false,
+                            backgroundColor: "#00a65a",
+                            borderColor: "#00a65a",
+                            label: '已完成行程数',
+                            data: [{{ implode(",",$transportChart['finishedRange']) }}]
+                        },
+                        {
+                            fill: false,
+                            backgroundColor: "#f5302e",
+                            borderColor: "#f5302e",
+                            label: '未完成行程数',
+                            data: [{{ implode(",",$transportChart['unFinishedRange']) }}]
+                        },
+                    ]
+                },
+                options: {
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false,
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero:true
+                            }
+                        }]
+                    }
+                }
+            });
+        });
+    </script>
 @endsection
