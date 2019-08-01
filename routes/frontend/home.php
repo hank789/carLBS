d577edfd2cb6d8bf56b7f6a2eb426e49b01689b7
hank <hank.huiwang@gmail.com>
2019-08-01 15:59:19 +0800

    update

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -17,7 +17,7 @@ Route::get('/openApp/{name}', [HomeController::class, 'openApp'])->name('openApp
 Route::get('contact', [ContactController::class, 'index'])->name('contact');
 Route::post('contact/send', [ContactController::class, 'send'])->name('contact.send');
 
-Route::get('/', [HomeController::class, 'expiredAlert'])->name('expiredAlert');
+Route::get('expiredAlert', [HomeController::class, 'expiredAlert'])->name('expiredAlert');
 
 /*
  * These frontend controllers require the user to be logged in
