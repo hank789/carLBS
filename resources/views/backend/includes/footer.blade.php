39c460f0cde84318631a9bf3d662adc67de70042
wanghui <hwang10@vova.com.hk>
2020-06-17 10:22:22 +0800

    改名

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="app-footer">
     <div class="ml-auto">
-        <strong>Copyright &copy; {{ date('Y') }} <a href="#">中讯智慧物联信息科技(苏州)有限公司</a></strong> @lang('strings.backend.general.all_rights_reserved')
+        <strong>Copyright &copy; {{ date('Y') }} <a href="#">常熟市盈联智能化工程有限公司</a></strong> @lang('strings.backend.general.all_rights_reserved')
     </div>
 </footer>
